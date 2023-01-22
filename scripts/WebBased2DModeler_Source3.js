37aa3edfcaf937fe4b3fb1ebe959724bae9b6027
Ryan C Blanchard <36056811+ryanblanchard237@users.noreply.github.com>
2023-01-22 15:34:09 -0800

    Add files via upload

---
+++ scripts/WebBased2DModeler_Source3.js
@@ -0,0 +1,27 @@
+class AddShapeState
+{
+    constructor()
+    {
+        this.startedAddingAShape = false;
+        
+        
+        
+        this.startedAddingAPoint = false;
+        
+        this.startedAddingALineSegment = false;
+        this.setLineSegmentFirstPoint = false;
+        
+        this.startedAddingAPolyline = false;
+        this.addedAPolylineInstance = false;
+        this.finishedAddingAPolyline = false;
+        
+        this.startedAddingAReferenceLine = false;
+        this.setReferenceLineFirstPoint = false;
+        
+        this.startedAddingARectangle = false;
+        this.setRectangleFirstCorner = false;
+        
+        this.startedAddingACircle = false;
+        this.setCircleCenter = false;
+    }
+}
